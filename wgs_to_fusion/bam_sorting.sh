8ee9842b7022c8331c4c59df5242ad75510b923d
Emily Isenhart <emily.isenhart@roswellpark.org>
2026-09-03 19:56:51 -0400

    add wgs

---
+++ wgs_to_fusion/bam_sorting.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#SBATCH --time=6:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=16G
+#SBATCH --job-name="bam_sorting"
+#SBATCH --output=logs/bam_sorting_%A_%a.out
+#SBATCH --mail-user=emilyise@buffalo.edu
+#SBATCH --mail-type=ALL
+#SBATCH --partition=general-compute
+#SBATCH --qos=general-compute
+
+###################################################
+## SET UP ##
+cd /projects/rpci/joyceohm/Emily/2026_Fusions_WGS_St_Jude/BAMS
+
+echo "loading modules" 
+module load gcc/11.2.0
+module load samtools/1.16.1
+
+###################################################
+## GET FILES ##
+CHUNK=$1
+
+file=$(sed -n "$((SLURM_ARRAY_TASK_ID+1))p" "$CHUNK")
+echo "Processing $file"
+
+###################################################
+## SORTING ##
+samtools sort -n -@ 8 \
+    -o "${file%.bam}.name_sorted.bam" \
+    "$file"
