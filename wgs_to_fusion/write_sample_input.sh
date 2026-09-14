01ab3c97f650b111c83649fd5080903e9dd16e53
Emily Isenhart <emily.isenhart@roswellpark.org>
2026-09-14 14:49:35 -0400

    updating for wgs

--- wgs_to_fusion/write_sample_input.sh
+++ wgs_to_fusion/write_sample_input.sh
@@ -5,23 +5,23 @@
 # THIS SCRIPT WAS PRIMARILY GENERATED WITH CHATGPT (I am occasionally very lazy)
 
 # Set directory
-P_DIR="/projects/rpci/joyceohm/Emily/2026_Fusions_St_Jude/BAMS"
+P_DIR="/projects/rpci/joyceohm/Emily/2026_Fusions_St_Jude/wgs_to_fusion/BAMS"
 cd $P_DIR
 
 # Output CSV file
 output="${P_DIR}/STJude26_sample_input.csv"
 
 # Write header
-echo "sample,fastq_1,fastq_2,strandedness" > "$output"
+echo "patient,sample,lane,fastq_1,fastq_2" > "$output"
 
 # Loop through all end1 files
 for r1_file in *.RNA-Seq.bam.end1.fq.gz; do
 
     # Extract sample ID by removing suffix
-    sample=$(basename "$r1_file" .RNA-Seq.bam.end1.fq.gz)
+    sample=$(basename "$r1_file" .WholeGenome.end1.fq.gz)
 
     # Define matching R2 file
-    r2_file="${sample}.RNA-Seq.bam.end2.fq.gz"
+    r2_file="${sample}.WholeGenome.end2.fq.gz"
 
     # Check that R2 exists
     if [[ -f "$r2_file" ]]; then
